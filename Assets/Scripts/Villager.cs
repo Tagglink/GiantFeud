2e4195a2dbd48bbc79754c0a6088911bb1e2ead9
Tomas Granlund <tagglink42@gmail.com>
2017-01-17 10:43:26 +0100

    Map & Villager scripts, Player script changes.

---
+++ Assets/Scripts/Villager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Villager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void WalkTo()
+    {
+        Map.tiles[0][0]
+    }
+}
