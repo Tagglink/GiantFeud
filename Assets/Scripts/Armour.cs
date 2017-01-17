a226466c011bc408230f0b80db877fa560c929f7
Nershahof <serpent.julle@gmail.com>
2017-01-17 15:11:41 +0100

    Further Item progress

--- Assets/Scripts/Armour.cs
+++ Assets/Scripts/Armour.cs
@@ -2,7 +2,11 @@
 using System.Collections;
 
 public class Armour : Equipment {
-    
 
+    public Armour(string _name, string _description, Resources _resourceCost, Sprite _icon, Stats _stats, int _reinforcementCount, Stats _reinforcementStats)
+        : base(_name, _description, _resourceCost, _icon, _stats, _reinforcementCount, _reinforcementStats)
+    {
+
+    }
 
 }
--- Assets/Scripts/Stats.cs
+++ Assets/Scripts/Stats.cs
@@ -7,6 +7,7 @@ public struct Stats {
     public int def;
     public int maxHP;
     public int hp;
+    public int hpPerSec;
     public int duration;
 
     public static Stats operator+(Stats stats1, Stats stats2)
@@ -17,7 +18,32 @@ public struct Stats {
         ret.def = stats1.def + stats2.def;
         ret.maxHP = stats1.maxHP + stats2.maxHP;
         ret.hp = stats1.hp + stats2.hp;
+        ret.hpPerSec = stats1.hpPerSec + stats2.hpPerSec;
         ret.duration = stats1.duration + stats2.duration;
         return ret;
+    }
+
+    public static Stats operator -(Stats stats1, Stats stats2)
+    {
+        Stats ret = new Stats();
+        ret.atk = stats1.atk - stats2.atk;
+        ret.atkspd = stats1.atkspd - stats2.atkspd;
+        ret.def = stats1.def - stats2.def;
+        ret.maxHP = stats1.maxHP - stats2.maxHP;
+        ret.hp = stats1.hp - stats2.hp;
+        ret.hpPerSec = stats1.hpPerSec - stats2.hpPerSec;
+        ret.duration = stats1.duration - stats2.duration;
+        return ret;
+    }
+
+    public Stats(int _atk, float _atkspd, int _def, int _maxHP, int _hp, int _hpPerSec, int _duration)
+    {
+        atk = _atk;
+        atkspd = _atkspd;
+        def = _def;
+        maxHP = _maxHP;
+        hp = _hp;
+        hpPerSec = _hpPerSec;
+        duration = _duration;
     }
 }
