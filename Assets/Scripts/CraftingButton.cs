082d8be8935e3fb46dba48a803db46380b49ab8d
Nershahof <serpent.julle@gmail.com>
2017-01-24 14:53:40 +0100

    HUD Animation Progress

---
+++ Assets/Scripts/CraftingButton.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class CraftingButton : MonoBehaviour {
+
+    private List<GameObject> children;
+    private List<Vector3> startPositions;
+    public bool retracted;
+    private bool lerping;
+    private float lerpTime;
+
+	void Start () {
+        lerpTime = 0;
+        retracted = true;
+        lerping = false;
+        children = GetChildren();
+        startPositions = GetPositions(children);
+        HardRetract(children);
+    }
+	
+	void Update () {
+	    if (lerping)
+        {
+            Move();
+
+            if (lerpTime >= 1)
+            {
+                retracted = !retracted;
+                lerping = false;
+                lerpTime = 0;
+            }
+        }
+	}
+
+    void Move()
+    {
+        for (int i = 0; i < children.Count; i++)
+        {
+            GameObject child = children[i];
+            RectTransform rectTransform = child.GetComponent<RectTransform>();
+            lerpTime += Time.deltaTime;
+
+            if (retracted)
+            {
+                rectTransform.localPosition = Vector3.Lerp(Vector3.zero, startPositions[i], 1 - Curve(lerpTime));
+                rectTransform.localScale = Vector3.Lerp(Vector3.zero, Vector3.one, 1 - Curve(lerpTime));
+            }
+            else
+            {
+                rectTransform.localPosition = Vector3.Lerp(startPositions[i], Vector3.zero, 1 - Curve(lerpTime));
+                rectTransform.localScale = Vector3.Lerp(Vector3.one, Vector3.zero, 1 - Curve(lerpTime));
+            }
+        }
+    }
+
+    public void ToggleMovement()
+    {
+        lerping = true;
+    }
+
+    List<GameObject> GetChildren()
+    {
+        List<GameObject> ret = new List<GameObject>();
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            ret.Add(transform.GetChild(i).gameObject);
+        }
+        return ret;
+    }
+
+    List<Vector3> GetPositions(List<GameObject> gameObjects)
+    {
+        List<Vector3> ret = new List<Vector3>();
+        foreach (GameObject a in gameObjects)
+        {
+            ret.Add(a.GetComponent<RectTransform>().localPosition);
+        }
+        return ret;
+    }
+
+    void HardRetract(List<GameObject> gameObjects)
+    {
+        foreach (GameObject a in gameObjects)
+        {
+            a.GetComponent<RectTransform>().localPosition = Vector3.zero;
+            a.GetComponent<RectTransform>().localScale = Vector3.zero;
+        }
+    }
+
+    float Curve(float time)
+    {
+        return (0.7f * time * time) - (1.7f * time) + 1;
+    }
+}
