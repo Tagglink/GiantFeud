842c8e728b806bf36f1397e81fa967e714cd7d98
Nershahof <serpent.julle@gmail.com>
2017-02-09 09:19:59 +0100

    Started Victory Screen Progress

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,21 +4,38 @@ using System.Collections;
 public class GameManager : MonoBehaviour {
 
     public GameObject playerObject;
+    public GameObject victoryScreen;
     private static Giant player;
 
 	void Start () {
         player = playerObject.GetComponent<Giant>();
 	}
 
-    public static void EndGame(Giant giant)
+    public void EndGame(Giant giant)
     {
+        bool loss;
         if (giant == player)
         {
             // Play Defeat Animation
+            loss = true;
         }
         else
         {
             // Play Victory Animation
+            loss = false;
+        }
+        PlayEndAnimation(loss);
+    }
+
+    void PlayEndAnimation(bool loss)
+    {
+        if (loss)
+        {
+            
+        }
+        else
+        {
+
         }
     }
 }
